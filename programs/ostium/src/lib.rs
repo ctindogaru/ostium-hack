6d6df44bcfd3bbff265bfe074d12cc5f933503f8
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-10-22 19:01:40 +0300

    build and deploy the project

--- programs/ostium/src/lib.rs
+++ programs/ostium/src/lib.rs
@@ -1,12 +1,12 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("DVCuZ7CgEi3WJrr1RMUhEP2eYW8PFKZXxw67RK9B9W6y");
 
 #[program]
 pub mod ostium {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
