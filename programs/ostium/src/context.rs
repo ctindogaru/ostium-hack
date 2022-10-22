a7314c57088092cc3baf16146cc6be4a31d99a04
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-10-22 19:14:00 +0300

    add all main methods

--- programs/ostium/src/context.rs
+++ programs/ostium/src/context.rs
@@ -2,3 +2,18 @@ use anchor_lang::prelude::*;
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Deposit {}
+
+#[derive(Accounts)]
+pub struct Withdraw {}
+
+#[derive(Accounts)]
+pub struct OpenPosition {}
+
+#[derive(Accounts)]
+pub struct ClosePosition {}
+
+#[derive(Accounts)]
+pub struct LiquidatePosition {}
--- programs/ostium/src/lib.rs
+++ programs/ostium/src/lib.rs
@@ -11,4 +11,24 @@ pub mod ostium {
     pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn deposit(_ctx: Context<Deposit>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn withdraw(_ctx: Context<Withdraw>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn open_position(_ctx: Context<OpenPosition>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn close_position(_ctx: Context<ClosePosition>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn liquidate_position(_ctx: Context<LiquidatePosition>) -> Result<()> {
+        Ok(())
+    }
 }
