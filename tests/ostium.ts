1ae03a0ff48efe03fd24b53afb0d75152595438f
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-10-26 18:24:48 +0300

    add test for deposit function

--- tests/ostium.ts
+++ tests/ostium.ts
@@ -1,6 +1,12 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { assert } from "chai";
+import {
+  createMint,
+  mintTo,
+  createAccount,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
 import { Ostium } from "../target/types/ostium";
 import { OSTIUM_SEED } from "./utils";
 const { SystemProgram } = anchor.web3;
@@ -8,11 +14,11 @@ const { SystemProgram } = anchor.web3;
 describe("ostium", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
+  const connection = provider.connection;
+  const wallet = provider.wallet;
   const program = anchor.workspace.Ostium as Program<Ostium>;
 
   it("initialize", async () => {
-    const wallet = provider.wallet;
-
     const [_, bump] = await anchor.web3.PublicKey.findProgramAddress(
       [OSTIUM_SEED],
       program.programId
@@ -35,5 +41,29 @@ describe("ostium", () => {
     assert.ok(stateAccount.admin.equals(wallet.publicKey));
   });
 
-  it("deposit", async () => {});
+  it("deposit", async () => {
+    const TOKEN_DECIMALS = 6;
+    const usdcOwner: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    await airdropSolTokens(connection, usdcOwner);
+    // let usdc = await createMint(
+    //   connection,
+    //   usdcOwner,
+    //   usdcOwner.publicKey,
+    //   null,
+    //   TOKEN_DECIMALS
+    // );
+  });
 });
+
+const airdropSolTokens = async (connection, wallet) => {
+  const airdrop_sig = await connection.requestAirdrop(
+    wallet.publicKey,
+    anchor.web3.LAMPORTS_PER_SOL
+  );
+  const latestBlockHash = await connection.getLatestBlockhash();
+  await connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: airdrop_sig,
+  });
+};
