d110493280047e6bf586caf7cfa9c70746d62def
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-10-27 12:06:32 +0300

    complete the test for deposit

--- tests/ostium.ts
+++ tests/ostium.ts
@@ -12,13 +12,15 @@ describe("ostium", () => {
   const connection = provider.connection;
   const wallet = provider.wallet;
   const program = anchor.workspace.Ostium as Program<Ostium>;
+  const state: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+  let pda;
+  let bump;
 
   it("initialize", async () => {
-    const [_, bump] = await anchor.web3.PublicKey.findProgramAddress(
+    [pda, bump] = await anchor.web3.PublicKey.findProgramAddress(
       [OSTIUM_SEED],
       program.programId
     );
-    const state: anchor.web3.Keypair = anchor.web3.Keypair.generate();
     await program.methods
       .initialize(bump)
       .accounts({
@@ -38,6 +40,9 @@ describe("ostium", () => {
 
   it("deposit", async () => {
     const TOKEN_DECIMALS = 6;
+    const TOKEN_MINT_AMOUNT = 1_000_000 * 10 ** TOKEN_DECIMALS;
+    const DEPOSIT_AMOUNT = 1_000 * 10 ** TOKEN_DECIMALS;
+
     const usdcOwner: anchor.web3.Keypair = anchor.web3.Keypair.generate();
     await airdropSolTokens(connection, usdcOwner);
     const usdc = await Token.createMint(
@@ -48,6 +53,32 @@ describe("ostium", () => {
       TOKEN_DECIMALS,
       TOKEN_PROGRAM_ID
     );
+    const usdcAccount = await usdc.createAccount(usdcOwner.publicKey);
+    await usdc.mintTo(usdcAccount, usdcOwner, [], TOKEN_MINT_AMOUNT);
+    const pdaAccount = await usdc.createAccount(pda);
+
+    let accountInfo;
+    accountInfo = await usdc.getAccountInfo(usdcAccount);
+    assert(accountInfo.amount == TOKEN_MINT_AMOUNT);
+    accountInfo = await usdc.getAccountInfo(pdaAccount);
+    assert(accountInfo.amount == 0);
+
+    await program.methods
+      .deposit(new anchor.BN(DEPOSIT_AMOUNT))
+      .accounts({
+        state: state.publicKey,
+        transferFrom: usdcAccount,
+        transferTo: pdaAccount,
+        authority: usdcOwner.publicKey,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      })
+      .signers([usdcOwner])
+      .rpc();
+
+    accountInfo = await usdc.getAccountInfo(usdcAccount);
+    assert(accountInfo.amount == TOKEN_MINT_AMOUNT - DEPOSIT_AMOUNT);
+    accountInfo = await usdc.getAccountInfo(pdaAccount);
+    assert(accountInfo.amount == DEPOSIT_AMOUNT);
   });
 });
 
