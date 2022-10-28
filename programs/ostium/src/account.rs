48603b02aa6a25154dfc1f9f8acc8fc0d1e8cd83
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-10-28 17:41:14 +0300

    add initial structs for position and positionmanager

--- programs/ostium/src/account.rs
+++ programs/ostium/src/account.rs
@@ -2,7 +2,21 @@ use anchor_lang::prelude::*;
 
 #[account]
 pub struct State {
+    pub is_initialized: bool,
     pub admin: Pubkey,
     pub bump_seed: u8,
+}
+
+#[account]
+pub struct Position {
     pub is_initialized: bool,
+    pub entry_price: u64,
+    pub amount: u64,
+    pub leverage: u8,
+}
+
+#[account]
+pub struct PositionManager {
+    pub is_initialized: bool,
+    pub no_of_positions: u64,
 }
